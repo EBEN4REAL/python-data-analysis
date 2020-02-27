ed61b1c710f55dc5b26151c9bd0188ba098b94dd
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-02-27 11:09:46 +0100

    ..

--- break.py
+++ break.py
@@ -71,3 +71,212 @@ for letter, num in zip(letters, nums):
 letters = ['a', 'b', 'c', 'd', 'e']
 for i, letter in enumerate(letters):
     print(i, letter)
+    
+    
+# Quiz: Zip Coordinates
+# Use zip to write a for loop that creates a string specifying the label and coordinates of each point and appends it to the list points. Each string should be formatted as label: x, y, z. For example, the string for the first coordinate should be F: 23, 677, 4.
+
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+# write your for loop here
+
+for x, y, z, label in zip(x_coord, y_coord, z_coord, labels):
+    points.append("{}: {}, {}, {}".format(label,x,y,z))
+    
+
+
+for point in points:
+    print(point)
+
+
+
+# Quiz: Zip Lists to a Dictionary
+# Use zip to create a dictionary cast that uses names as keys and heights as values.
+
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+print(zip(cast_names, cast_heights))
+
+cast = dict(zip(cast_names, cast_heights))
+print(cast)
+
+
+# Quiz Solution: Unzip Tuples
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+names, heights = zip(*cast)
+print(names)
+print(heights)
+
+
+# Quiz Solution: Transpose with Zip
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = tuple(zip(*data))
+print(data_transpose)
+
+
+# Quiz Solution: Enumerate
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+for i, character in enumerate(cast):
+    cast[i] = character + " " + str(heights[i])
+
+print(cast)
+
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+
+first_names = [name.split()[0].lower() for name in names]
+print(first_names)
+
+# Quiz Solution: Filter Names by Scores
+scores = {
+            "Rick Sanchez": 70,
+            "Morty Smith": 35,
+            "Summer Smith": 82,
+            "Jerry Smith": 23,
+            "Beth Smith": 98
+          }
+
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
+
+
+number_list = [x ** 2 for x in range(10) if x % 2 == 0]
+print(number_list)
+
+
+
+# write your function here
+def readable_timedelta(days):
+    weeks = int(days / 7)
+    days = days % 7
+    str = "{} week(s) and {} day(s)".format(weeks,days)
+    return str
+
+# test your function
+print(readable_timedelta(10))
+
+
+numbers = [
+              [34, 63, 88, 71, 29],
+              [90, 78, 51, 27, 45],
+              [63, 37, 85, 46, 22],
+              [51, 22, 34, 11, 18]
+           ]
+
+def mean(num_list):
+    return sum(num_list) / len(num_list)
+
+averages = list(map(mean, numbers))
+print(averages)
+
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+print(y)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+
+class Person:
+      def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+# The map() Function
+# The advantage of the lambda operator can be seen when it is used in combination with the map() function.
+# map() is a function with two arguments:
+# r = map(func, seq)
+# The first argument func is the name of a function and the second a sequence (e.g. a list) seq. map() applies the function func to all the elements of the sequence seq. It returns a new list with the elements changed by func
+
+def fahrenheit(T):
+    return ((float(9)/5)*T + 32)
+def celsius(T):
+    return (float(5)/9)*(T-32)
+temp = (36.5, 37, 37.5,39)
+
+F = map(fahrenheit, temp)
+C = map(celsius, F)
+
+print(list(F))
+print(list(C))
+
+# Filtering
+# The function filter(function, list) offers an elegant way to filter out all the elements of a list, for which the function function returns True.
+# The function filter(f,l) needs a function f as its first argument. f returns a Boolean value, i.e. either True or False. This function will be applied to every element of the list l. Only if f returns True will the element of the list be included in the result list.
+fib = [0,1,1,2,3,5,8,13,21,34,55]
+result = filter(lambda x: x % 2, fib)
+print(list(result))
+[1, 1, 3, 5, 13, 21, 55]
+
+result = filter(lambda x: x % 2 == 0, fib)
+print (list(result))
+[0, 2, 8, 34]
+
+# Quiz Solution: Lambda with Filter
+cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)
+
+
+# Quiz Solution: Implement my_enumerate (Generaor Function)
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
+    
+def simpleGeneratorFun(): 
+    yield 1
+    yield 2
+    yield 3
+  
+# Driver code to check above generator function 
+for value in simpleGeneratorFun():  
+    print(value) 
+    
+sq_list = [x**2 for x in range(10)]  # this produces a list of squares
+print(sq_list)
+
+sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
+print(list(sq_iterator))
